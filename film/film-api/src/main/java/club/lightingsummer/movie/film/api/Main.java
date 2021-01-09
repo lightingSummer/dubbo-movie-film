916728e1866d49c71529b4aa49b74110d0d4394f
lightingSummer <1356221057@qq.com>
2021-01-09 11:56:52 +0800

    structure

--- film/film-api/src/main/java/club/lightingsummer/movie/film/api/Main.java
+++
@@ -1,9 +0,0 @@
-package club.lightingsummer.movie.film.api;
-
-/**
- * @author     ：lightingSummer
- * @date       ：2019/7/1 0001
- * @description：
- */
-public class Main {
-}
--- film/film-dal/src/main/java/club/lightingsummer/movie/film/dal/Main.java
+++
@@ -1,9 +0,0 @@
-package club.lightingsummer.movie.film.dal;
-
-/**
- * @author     ：lightingSummer
- * @date       ：2019/7/1 0001
- * @description：
- */
-public class Main {
-}
